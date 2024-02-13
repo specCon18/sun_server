0b724de7bacaf72f7a44e9326c467fd2cd995c4e
Steven <specCon18@gmail.com>
2024-02-13 15:15:54 -0500

    fixed port

--- nixosModules/sunServer/default.nix
+++ nixosModules/sunServer/default.nix
@@ -29,7 +29,7 @@ in{
     config = mkIf cfg.enable {
         networking = {
           firewall = {
-            # allowedTCPPorts = [];
+            allowedTCPPorts = [cfg.port];
             allowedUDPPorts = [cfg.port];
           };
         };
