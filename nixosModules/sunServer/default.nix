f957fae418d0fa45d1993078ab9bc4f7ea3261da
Steven <specCon18@gmail.com>
2024-02-13 14:53:31 -0500

    added optional attrs

--- nixosModules/sunServer/default.nix
+++ nixosModules/sunServer/default.nix
@@ -2,7 +2,7 @@
 let
     # resources used to make
     # https://nixos.wiki/wiki/Extend_NixOS
-    inherit (lib) mkIf mkOption mkEnableOption optionalAttrs types;
+    inherit (lib) mkIf mkOption mkEnableOption optionalAttrs types literalExpression;
     cfg = config.services.sunServer;
 in{
     options = {
